a4e59f4cc00763e5e87337ac81ba196df497fe8e
ChanMin <park.c@husky.neu.edu>
2019-06-16 20:21:33 -0400

    image category recommendation for user

--- src/main/java/space_forum_server/java_server/controllers/ImageController.java
+++ src/main/java/space_forum_server/java_server/controllers/ImageController.java
@@ -1,7 +1,11 @@
 package space_forum_server.java_server.controllers;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,24 +14,24 @@ import space_forum_server.java_server.models.*;
 
 @RestController
 public class ImageController {
-//  @CrossOrigin(origins = "*")
-//  @GetMapping("/api/images/recommendCategory/{sessionid}")
-//  public List<ForumThread> findAllThreads(@PathVariable("sessionid") String sessionid) {
-//    ForumThread ft = new ForumThread();
-//    UserController uc = new UserController();
-//    List<Image> likedImgs = uc.authenticateUser(sessionid).getLikedImages();
-//
-//    for(Image img : likedImgs){
-//      img.
-//    }
-//
-//    List<ForumThread> allThreads = (List<ForumThread>)forumThreadRepository.findAll();
-//    List<ForumThread> filteredThreads = new ArrayList<ForumThread>();
-//    for(ForumThread ft : allThreads){
-//      if(ft.getImage().getId() == imageid){
-//        filteredThreads.add(ft);
-//      }
-//    }
-//    return filteredThreads;
-//  }
+  @CrossOrigin(origins = "*")
+  @GetMapping("/api/images/recommendCategory/{sessionid}")
+  public String findAllThreads(@PathVariable("sessionid") String sessionid) {
+    UserController uc = new UserController();
+    List<Image> likedImgs = uc.authenticateUser(sessionid).getLikedImages();
+
+    HashMap<String, Integer> likedByUser = new HashMap<>();
+
+    for(Image img : likedImgs){
+      String key = img.getCategory();
+      if(likedByUser.containsKey(key)){
+        int val = likedByUser.get(key);
+        val++;
+      } else {
+        likedByUser.put(key, 1);
+      }
+    }
+
+    return Collections.max(likedByUser.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+  }
 }
